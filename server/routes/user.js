0125fbd2c3e7c8ba5cd48a0a199833f39a43f189
kenny-tech <kenny4real2001@gmail.com>
2018-09-26 15:50:41 +0100

    Create register route

---
+++ server/routes/user.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const validateRegisterInput = require('../validation/register');
+const validateLoginInpu = require('../validation/login');
+
+const User = require('../models/User');
+
+router.post('/register', function(req, res) {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    User.findOne({
+        email: req.body.email
+    }).then(user => {
+        if(user) {
+            return res.status(400).json({
+                email: 'Email already exists'
+            });
+        }
+        else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                if(err) console.log('There was an error', err);
+                else {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) console.log('There was an error', err);
+                        else {
+                            newUser.password = hash;
+                            newUser
+                                .save()
+                                .then(user => {
+                                    res.json(user)
+                                });
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
