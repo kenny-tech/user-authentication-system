0249bddec0545c5a9357efa06a91731ff29d461b
kenny-tech <kenny4real2001@gmail.com>
2018-09-26 16:11:41 +0100

    Create login route

--- server/routes/user.js
+++ server/routes/user.js
@@ -49,3 +49,47 @@ router.post('/register', function(req, res) {
         }
     });
 });
+
+router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+        .then(user => {
+            if(!user) {
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
+            }
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        const payload = {
+                            id: user.id,
+                            name: user.name
+                        }
+                        jwt.sign(payload, 'secret', {
+                            expiresIn: 3600
+                        }, (err, token) => {
+                            if(err) console.error('There is some error in token', err);
+                            else {
+                                res.json({
+                                    success: true,
+                                    token: `Bearer ${token}`
+                                });
+                            }
+                        });
+                    }
+                    else {
+                        errors.password = 'Incorrect Password';
+                        return res.status(400).json(errors);
+                    }
+                
+                });
+        });
+})
