8de119597543ffa88c922acb57e5577c9645a825
kenny-tech <kenny4real2001@gmail.com>
2018-10-09 16:47:26 +0100

    Modify index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
+import History from './history';
+import Routes from './routes';
+import { AUTH_USER } from './actions/types';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import registerServiceWorker from './serviceWorker';
+
+/*
+// UNCOMMENT IT FOR PRODUCTION
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+*/
+
+/* COMMENT IT OUT FOR PRODUCTION */
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(reduxThunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    )  
+);
+//
+
+const token = localStorage.getItem('token');
+// if we have a token, consider the user to be signed in
+if (token) {
+    // update application state
+    store.dispatch({ type: AUTH_USER });
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Router history={History}>
+                <Switch>
+                    <Routes />
+                </Switch>
+            </Router>
+        </BrowserRouter>
+    </Provider>, document.getElementById('root'));
+registerServiceWorker();
