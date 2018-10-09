432a60f05841b6719c6876ba5a74400552775a74
kenny-tech <kenny4real2001@gmail.com>
2018-10-09 12:45:17 +0100

    Create App.js Component

---
+++ client/src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="container" style={{ marginTop: '50px' }}>
+                    <div className="row">
+                        <div className="col-md-12">
+                            {this.props.children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
